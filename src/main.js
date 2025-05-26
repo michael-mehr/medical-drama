63282d024614def8884c2e75b8fcec3c6641dd07
Michael Mehr <michaelmehr0@gmail.com>
2025-05-26 16:16:13 -0500

    selected buttons + frame in main.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const sideButtons = document.querySelectorAll("div.side-buttons > button");
+const subButtons = document.querySelectorAll("div.sub-buttons > button");
+const mainButtons = document.querySelectorAll("div.main-buttons > button");
+const directionButtons = document.querySelectorAll("div.direction-buttons > button");
+
+const mainFrame = document.querySelector("div.frame");
