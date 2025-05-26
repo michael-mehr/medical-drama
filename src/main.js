a6f792b797a48da2423e947f03e00bbdad9b5506
Michael Mehr <michaelmehr0@gmail.com>
2025-05-26 16:52:31 -0500

    event handler for main buttons

--- src/main.js
+++ src/main.js
@@ -4,3 +4,12 @@ const mainButtons = document.querySelectorAll("div.main-buttons > button");
 const directionButtons = document.querySelectorAll("div.direction-buttons > button");
 
 const mainFrame = document.querySelector("div.frame");
+
+function handleMainButtons(e) {
+  console.log(e.currentTarget.textContent);
+}
+
+for (const button of mainButtons) {
+  button.addEventListener("click", handleMainButtons);
+}
+
